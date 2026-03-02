ae3d097a76f99a4a91ae02cd0b2e5440cd59c56c
Liehan Els <lels@iie.ac.za>
2026-03-02 09:56:50 +0200

    Please improve the password checking logic to make this page more secure!

--- app/src/main/java/com/example/passwordapp/MainActivity.kt
+++ app/src/main/java/com/example/passwordapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.passwordapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,37 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val tvTitle = findViewById<TextView>(R.id.tvMainTitle)
+        val edtUsername = findViewById<EditText>(R.id.edtTextMainUsername)
+        val edtPassword = findViewById<EditText>(R.id.edtPwMainPassword)
+        val edtConfirmPassword = findViewById<EditText>(R.id.edtPwMainConfirmPassword)
+        val btnLogin = findViewById<Button>(R.id.btnMainLogin)
+
+        var username: String
+        var password: String
+        var confirmPassword: String
+
+        btnLogin.setOnClickListener()
+        {
+            username = edtUsername.text.toString()
+            password = edtPassword.text.toString()
+            confirmPassword = edtConfirmPassword.text.toString()
+
+            if(username.length > 8 || username.length == 8)
+            {
+                if(password != confirmPassword || password.isEmpty())
+                {
+                    tvTitle.text = "Passwords incorrect"
+                }
+                else
+                {
+                    tvTitle.text = "Login Successful"
+                }
+            }
+            else {
+                tvTitle.text = "Username must be at least 8 characters"
+            }
         }
     }
 }
